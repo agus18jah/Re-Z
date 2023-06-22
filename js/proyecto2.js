2e9f104e40cc1a104686fd9d5142b109b97d5a70
Agustin Garcia <132966901+agus18jah@users.noreply.github.com>
2023-06-22 13:15:54 -0300

    Add files via upload

---
+++ js/proyecto2.js
@@ -0,0 +1,10 @@
+alert ("Bienvenidxs al grandioso mundo del Dulce de Leche")
+
+function cambiotema() {
+    document.body.style.backgroundColor = "black";
+    document.body.style.color = "white";
+} 
+
+function copyright() {
+    alert ("Autor: Agustin Garcia ---- Email: agus18jah@gmail.com")
+}
