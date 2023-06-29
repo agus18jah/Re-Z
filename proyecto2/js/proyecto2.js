56829d34cfa2bd58aa7279bb2a1ac23a8ecfcb80
Agustin Garcia <132966901+agus18jah@users.noreply.github.com>
2023-06-29 13:15:45 -0300

    Add files via upload

---
+++ proyecto2/js/proyecto2.js
@@ -0,0 +1,22 @@
+function cambiotema() {
+    document.body.style.backgroundColor = "black";
+    document.body.style.color = "white";
+    document.getElementById("inclusivo").style.backgroundColor = "black";
+    document.getElementById("inclusivo").style.color = "white";
+    document.getElementById("principal").style.backgroundColor = "black";
+    document.getElementById("principal").style.color = "white";
+    document.getElementById("titulo2").style.backgroundColor = "black";
+    document.getElementById("titulo2").style.color = "white";
+    document.getElementById("ingredientes").style.backgroundColor = "black";
+    document.getElementById("ingredientes").style.color = "white";
+    document.getElementById("pasos").style.backgroundColor = "black";
+    document.getElementById("pasos").style.color = "white";
+    document.getElementById("info").style.backgroundColor = "black";
+    document.getElementById("info").style.color = "white";
+    document.getElementById("origen").style.backgroundColor = "black";
+    document.getElementById("origen").style.color = "white";
+} 
+
+function copyright() {
+    alert ("Autor: Agustin Garcia ---- Email: agus18jah@gmail.com")
+}
